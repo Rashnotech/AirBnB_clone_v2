3dcf31a28186680bcafc77bbdeaacd3f2cc7ea6a
Rashnotech <rashnotech@gmail.com>
2023-09-18 12:50:13 +0000

    Update: fixed basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -24,10 +24,13 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            for attr, value in kwargs.items():
+                if attr == 'updated_at' or attr == 'created_at':
+                    setattr(self, attr, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif attr == '__class__':
+                    pass
+                else:
+                    setattr(self, attr, value)
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
