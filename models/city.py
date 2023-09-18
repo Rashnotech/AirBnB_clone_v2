e6cc544b5feed9c789ada8ad370ebc85557c0bd0
Rashnotech <rashnotech@gmail.com>
2023-09-18 10:54:02 +0000

    Fixed: used backref instead of back_populates

--- models/city.py
+++ models/city.py
@@ -12,5 +12,4 @@ class City(BaseModel, Base):
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
-    state = relationship('State', back_populates='cities')
-    places = relationship('Place', back_populates='cities', cascade='all, delete')
+    places = relationship('Place', backref='cities', cascade='all, delete')
