1779c2eafa0d552f084d605992b75ca0ca3546f2
Initech <inimfonebong001@gmail.com>
2023-09-19 08:00:26 -0700

    models/place.py

--- models/place.py
+++ models/place.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, Integer, Float, String
 from sqlalchemy.orm import relationship
+from os import environ
 
 
 class Place(BaseModel, Base):
@@ -11,7 +12,7 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
@@ -19,3 +20,15 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
     amenity_ids = []
+    reviews = relationship('Review', backref='place', cascade='all, delete')
+
+
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def reviews(self):
+            """getter function for reviews"""
+            rev_list = []
+            for review in list(storage.all(Review).values()):
+                if review.place_id == self.id:
+                    rev_list.append(review)
+            return rev_list
