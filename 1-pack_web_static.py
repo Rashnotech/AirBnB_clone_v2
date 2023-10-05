c8e8b12a2b92596b196eb6c5ce8caa79797d018d
Rashnotech <rashnotech@gmail.com>
2023-10-05 00:16:41 +0000

    Archiving a file using fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a module that generate an archive """
+from fabric.api import local, run, put
+from datetime import datetime
+
+
+def do_pack():
+    """ a function that compress file and stop them in an archive """
+    try:
+        time = datetime.now()
+        timestamp = time.strftime("%Y%m%d%H%M%S")
+        archive = "versions/web_static_{}.tgz".format(timestamp)
+        local("mkdir -p versions")
+        local("tar -czf {} web_static".format(archive))
+        return archive
+    except Exception as e:
+        return None
