78c29cc6636254c9dd737a342aa31feb245aa5ff
Rashnotech <rashnotech@gmail.com>
2023-10-06 16:38:50 -0700

    Code revamped

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,57 +1,57 @@
 # puppet script that sets up your web servers for deploymenta
 
 exec { 'install nginx':
-command => '/usr/bin/apt -y update; /usr/bin/apt -y install nginx',
+  command => 'apt -y update && apt -y install nginx',
 }
 
-exec { 'create directories':
-command  => 'mkdir -p /data/web_static/releases/test/; mkdir -p /data/web_static/shared/',
-provider => shell,
+exec { 'make directories':
+  command  => 'mkdir -p /data/web_static/releases/test/; mkdir -p /data/web_static/shared/',
+  provider => shell,
 }
 
-exec { 'write':
-command  => 'echo "hello world!" | sudo tee /data/web_static/releases/test/index.html',
-require  => Exec['install nginx'],
-provider => shell,
+file { '/data/web_static/releases/test/index.html':
+  ensure  => file,
+  content => 'Holberton school',
+  require => Exec['install nginx'],
 }
 
 exec { 'link':
-command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
-require  => Exec['create directories'],
-provider => shell,
+  command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  require  => Exec['make directories'],
+  provider => shell,
 }
 
-exec { 'change owner':
-command  => 'chown -R ubuntu:ubuntu /data/',
-require  => Exec['create directories'],
-provider => shell,
+exec { 'owner':
+  command  => 'chown -R ubuntu:ubuntu /data/',
+  require  => Exec['make directories'],
+  provider => shell,
 }
 
 
-exec {'configure server':
-command  => 'printf %s "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	add_header X-Served-By $HOSTNAME;
-	root /var/www/html;
-	index index.html index.htm;
-
-	location /hbnb_static {
-		alias /data/web_static/current;
-		index index.html index.htm;
-	}
-	rewrite ^/redirect_me https://google.com;
-	error_page 404 /404.html;
-	location /404 {
-		root /var/www/html;
-		internal;
-	}
-}" > /etc/nginx/sites-available/default',
-require  => Exec['install nginx'],
-provider => shell,
+file {'/etc/nginx/sites-available/default':
+  ensure   => file,
+  content  => "
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+      add_header X-Served-By $hostname;
+      root /var/www/html;
+      index index.html index.htm;
+      location /hbnb_static {
+      alias /data/web_static/current;
+ 	index index.html index.htm;
+      }
+      rewrite ^/redirect_me https://google.com;
+      error_page 404 /404.html;
+      location /404 {
+  	root /var/www/html;
+	internal;
+      }
+  }",
+  require  => Exec['install nginx'],
 }
 
 exec { 'run':
-command  => 'service nginx restart',
-provider => shell,
+  command  => 'service nginx restart',
+  provider => shell,
 }
