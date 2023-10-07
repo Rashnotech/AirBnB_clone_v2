3ab153ac9c4c3947cf4fea8d5b3d25233cc3a95b
Rashnotech <rashnotech@gmail.com>
2023-10-07 05:00:41 +0000

    Clean up

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """a module that deletes out out of date archives """
-from fabric.api import local, cd, lcd, env, run
 import os
+from fabric.api import run, local, env, lcd, cd
 
 
 env.hosts = ["35.174.208.242", "54.162.47.71"]
@@ -14,25 +14,27 @@ def do_clean(number=0):
     Args:
       number: variable that keeps the most recent archives
     """
+
+
     number = int(number)
     if number == 0:
         number = 1
         print(number)
 
     archives = sorted(os.listdir("versions"))
-    for _ in range(number):
+    for i in range(number):
         archives.pop()
     with lcd("versions"):
         for arc in archives:
             local("rm {}".format(arc))
 
     with cd("/data/web_static/releases"):
-        archives = run("ls -ltr").split()
-        for arc in archives:
+        archieves = run("ls -tr").split()
+        for arc in archieves:
             if "web_static_" in arc:
-                archives = arc
-        archives = list(archives)
-        for _ in range(number):
-            archives.pop()
-        for arc in archives:
+                archieves = arc
+        archieves = list(archieves)
+        for i in range(number):
+            archieves.pop()
+        for arc in archieves:
             run("rm -rf {}".format(arc))
